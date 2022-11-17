976d4fd85ec2261d5359417b108af2876e32787f
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-11-16 21:33:47 -0300

    Finalizando o HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -15,7 +15,7 @@ export function HomeHeader() {
         size={16}
         mr={4}
       />
-      <VStack>
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
