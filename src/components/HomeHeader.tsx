50608fa98667b4f0842d9f767b2eafcea856d712
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-11-16 21:19:57 -0300

    Estilizando o HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,11 +2,15 @@ import { Heading, HStack, Text, VStack } from 'native-base';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">Olá,</Text>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
 
-        <Heading color="gray.100">Rodrigo</Heading>
+        <Heading color="gray.100" fontSize="md">
+          Rodrigo
+        </Heading>
       </VStack>
     </HStack>
   );
