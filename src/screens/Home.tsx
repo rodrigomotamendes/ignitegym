bd2418f601ef94e0f9ba2e6abd2ad2180ff3b1c3
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-11-17 14:53:06 -0300

    Header da lista de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { VStack, FlatList } from 'native-base';
+import { VStack, FlatList, HStack, Heading, Text } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
@@ -28,6 +28,18 @@ export function Home() {
         my={10}
         maxHeight={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
