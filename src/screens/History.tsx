0e4e3c049a7f09ed52424f7ef605751732bbdb6d
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-12-19 22:51:42 -0300

    Mensagem de lista vazia

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,4 +1,4 @@
-import { Heading, VStack, SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 import { useState } from 'react';
@@ -29,6 +29,15 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda.{'\n'} Vamos fazer exercício
+            hoje?
+          </Text>
+        )}
       />
     </VStack>
   );
