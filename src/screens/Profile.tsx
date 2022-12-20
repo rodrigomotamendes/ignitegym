12abf5f5569edfa4e238640581f5ba3161c4ce2b
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-12-20 14:38:28 -0300

    Recap da Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -23,7 +23,7 @@ export function Profile() {
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -56,10 +56,14 @@ export function Profile() {
           <Input bg="gray.600" placeholder="Nome" />
 
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
-        </Center>
 
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
             Alterar senha
           </Heading>
 
@@ -74,7 +78,7 @@ export function Profile() {
           />
 
           <Button title="Atualizar" mt={4} />
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   );
