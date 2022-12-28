edcc258daa967c90226c6b791749d6fdcfb6fa51
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-12-28 17:42:29 -0300

    Utilizando mensagens toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Alert, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import {
   Center,
   ScrollView,
@@ -7,6 +7,7 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
@@ -23,6 +24,8 @@ export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
     'https://github.com/rodrigomotamendes.png'
   );
+
+  const toast = useToast();
 
   async function handleUserPhotoSelect() {
     setPhotoIsLoading(true);
@@ -45,9 +48,11 @@ export function Profile() {
         );
 
         if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
-          return Alert.alert(
-            'Essa imagem é muito grande. Escolha uma de até 5MB.'
-          );
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
         }
 
         setUserPhoto(photoSelected.assets[0].uri);
