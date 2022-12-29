3ee985e450da160101b7c72ad1bf002d77894d8c
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-12-28 22:30:48 -0300

    Validando confirmação da senha

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -24,6 +24,10 @@ const signUpSchema = yup.object({
     .string()
     .required('Informe a senha')
     .min(6, 'A senha deve ter pelo menos 6 dígitos'),
+  password_confirm: yup
+    .string()
+    .required('Confirme a senha')
+    .oneOf([yup.ref('password'), null], 'A confirmação da senha não confere'),
 });
 
 export function SignUp() {
@@ -126,6 +130,7 @@ export function SignUp() {
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password_confirm?.message}
                 onSubmitEditing={handleSubmit(handleSignUp)}
                 returnKeyType="send"
               />
