f084e9e5e5d17c3541b0cfca00ece6fe4a6b2e15
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2023-01-04 23:02:36 -0300

    Axios Exceptions

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -4,6 +4,7 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
+import axios from 'axios';
 import { api } from '@services/api';
 
 import LogoSvg from '@assets/logo.svg';
@@ -11,6 +12,7 @@ import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { Alert } from 'react-native';
 
 type FormDataProps = {
   name: string;
@@ -46,9 +48,14 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post('/users', { name, email, password });
-
-    console.log(response.data);
+    try {
+      const response = await api.post('/users', { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
   }
 
   return (
