98a5d33c743370f522095a813f11413483c8309d
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2023-01-04 23:13:21 -0300

    Entendendo Interceptors

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://192.168.0.2:3333',
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log('INTERCEPTOR RESPONSE ERROR', error);
+    return Promise.reject(error);
+  }
+);
+
+export { api };
