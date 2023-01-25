d2ee6f6b8067c3ca11414ca60d31f096ec245c01
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2023-01-24 22:13:09 -0300

    Removendo o token do storage

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -3,6 +3,7 @@ import { createContext, ReactNode, useEffect, useState } from 'react';
 import {
   storageAuthTokenSave,
   storageAuthTokenGet,
+  storageAuthTokenRemove,
 } from '@storage/storageAuthToken';
 import {
   storageUserSave,
@@ -72,6 +73,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setUser({} as UserDTO);
 
       await storageUserRemove();
+      await storageAuthTokenRemove();
     } catch (error) {
       throw error;
     } finally {
